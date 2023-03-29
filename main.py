0bb33bd5f80d6f0fed42c2c170ed5adb48c8caef
@dmytro.bobyk <dmytro.bobyk@pettersonapps.com>
2023-03-29 11:39:22 +0300

    utils for printing Phi1Phi2 results

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from os import path
 from traceback import print_exc
 from PIL import Image
 from calculations import image_Phi1_Phi2
+from utils import Phi1Phi2_info_print
 
 if __name__ == "__main__":
     try:
+        # setting images names and paths
         imagesFolder = 'assets/'
 
         standartImageNames = ['a.bmp', 'b.bmp', 'f.bmp', 'm.bmp']
@@ -14,20 +16,16 @@ if __name__ == "__main__":
         imageToClassifyInfo = {imageToClassifyName : path.abspath(f'{imagesFolder + imageToClassifyName}') for imageToClassifyName in imageToClassifyNames}
 
         imagesInfo = dict(standartImageInfo, **imageToClassifyInfo)
-        Phi1Phi2 = {}
 
+        # getting Phi1, Phi2
+        Phi1Phi2 = {}
         for imageName, imagePath in imagesInfo.items():
             with Image.open(imagePath) as im:
                 im = im.convert("L")
                 Phi1Phi2[imageName] = image_Phi1_Phi2(im)
 
-        noDrift = 1 
-        for key in imagesInfo.keys():
-            if noDrift and key in imageToClassifyNames:
-                noDrift = 0
-                print()
-            print(key, Phi1Phi2[key])
-
+        # printing Phi1, Phi2 results
+        Phi1Phi2_info_print(Phi1Phi2, standartImageNames + imageToClassifyNames, standartImageNames)
     except Exception as e:
         print('Error occured:')
         print_exc()
---
+++ utils.py
@@ -0,0 +1,16 @@
+from typing import Dict, List
+
+def Phi1Phi2_head_print():
+    print(f"{'Image name':<11}|{'Phi1':<22}|{'Phi2':<22}")
+
+def Phi1Phi2_info_print(Phi1Phi2: Dict[str, float], imageNames: List[str], standartImageNames: List[str]):
+
+    noDrtift = 1
+    print("Standart images:")
+    Phi1Phi2_head_print()
+    for imageName in imageNames:
+        if (noDrtift and imageName not in standartImageNames):
+            noDrtift = 0
+            print("\nImages to classify:")
+            Phi1Phi2_head_print()
+        print(f"{imageName:<11}|{Phi1Phi2[imageName]['Phi1']:<22}|{Phi1Phi2[imageName]['Phi2']:<22}")
